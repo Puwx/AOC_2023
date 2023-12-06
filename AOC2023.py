4212f5d6b80da08c2c5758f53b55eb1958315ea3
Matthew Kemper <37680606+Puwx@users.noreply.github.com>
2023-12-06 11:09:03 -0700

    Update AOC2023.py

--- AOC2023.py
+++ AOC2023.py
@@ -21,3 +21,60 @@ noTxt = [[replChar(ln)[0][0],replChar(ln)[-1][0]] for ln in ip]
 
 final = [int("{}{}".format(ln[0],ln[-1])) for ln in noTxt]
 sum(final)
+
+#Day 2
+ip = open("day2.txt","r").readlines()
+splt = re.compile(r"[,;]")
+cutOff = {"red":12,"green":13,"blue":14}
+
+# Part 1
+def makeCntDict(gmList):
+    clrDict = defaultdict(int)
+    for c,b in gmList:
+        b = int(b)
+        clrDict[c]+=b
+    return clrDict
+
+games = [[int(ln.split(":")[0].split()[-1]),[gm.strip().split()[::-1] for gm in re.split(splt,ln.split(":")[-1].strip())]] for ln in ip]
+
+"""
+games above is formatted like this:
+[[1, # Game ID
+  [['blue', '7'], #Contents
+   ['green', '6'],
+   ['red', '3'],
+   ['red', '3'],
+   ['green', '5'],
+   ['blue', '1'],
+   ['red', '1'],
+   ['green', '5'],
+   ['blue', '8'],
+   ['red', '3'],
+   ['green', '1'],
+   ['blue', '5']]],
+
+"""
+sum([idx for idx,gm in games if all([int(b)<=cutOff[c] for c,b in gm ])])
+
+# Part 2
+
+games = [[int(gm.split(":")[0].split()[-1]),[dict([bc.strip().split()[::-1] for bc in grp.split(",")]) for grp in gm.split(":")[-1].strip().split(";") ]] for gm in ip]
+
+prods = []
+for idx,gm in games:
+    clrDict = defaultdict(list)
+    for g in gm:
+        for c,b in g.items():
+            clrDict[c].append(int(b))
+    prods.append(np.prod([max(i) for _,i in clrDict.items()]))
+        
+"""
+games above is formatted like this:
+[[1, #Game ID
+  [{'blue': '7', 'green': '6', 'red': '3'}, #Contents
+   {'red': '3', 'green': '5', 'blue': '1'},
+   {'red': '1', 'green': '5', 'blue': '8'},
+   {'red': '3', 'green': '1', 'blue': '5'}]],
+   
+"""
+sum(prods)
